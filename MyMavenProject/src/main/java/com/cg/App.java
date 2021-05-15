8e0228bc29e6149b8fb4f2d73afebc490928d733
Milind23Ghawale <69033412+Milind23Ghawale@users.noreply.github.com>
2021-05-15 13:24:39 +0530

    Add files via upload

---
+++ MyMavenProject/src/main/java/com/cg/App.java
@@ -0,0 +1,17 @@
+package com.cg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Employee e1= new Employee(101,"Ram",45000);
+        
+        System.out.println(e1.toString());
+    }
+}
